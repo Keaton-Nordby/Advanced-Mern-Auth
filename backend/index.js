7da3ac0895db538e06c90b5683b0739b58c6d434
Keaton Nordby <keatonnordby4@gmail.com>
2025-09-23 15:13:08 -0500

    sign up route with auth and security measures

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import dotenv from "dotenv";
 
-const app = express()
+import { connectDB } from './db/connectDB.js';
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json()); // parses incoming requests from the req.body
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+
+app.use("/api/auth", authRoutes);
 
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Serve is running on port: ${PORT}`);
+});
+
+
+
+
+
